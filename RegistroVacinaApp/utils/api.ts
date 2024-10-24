befba96c459fa827f748a3835d9779e9976dc300
Stephany Milhomem <stephanymilhomem25@gmail.com>
2024-10-24 11:59:33 -0300

    Update .gitgnore

--- RegistroVacinaApp/utils/api.ts
+++ RegistroVacinaApp/utils/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-// Cria uma instância do Axios com a URL base da API
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',  // URL base da API
-  timeout: 10000,  // Tempo limite de 10 segundos
-  headers: {
-    'Content-Type': 'application/json',  // Cabeçalho padrão
-  },
+  baseURL: 'http://localhost:8080/api',
 });
+
+export default api;
